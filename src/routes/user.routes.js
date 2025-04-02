4a755293a0c74d2feedaef53a340d02d83f06a33
saikishore0319 <saikishor20172017@gmail.com>
2025-04-02 13:30:45 +0530

    added user route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import { reigsterUser } from "../controllers/user.controller.js";
+import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
 const userRouter = Router()
-userRouter.route("/register").post(reigsterUser)
+userRouter.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name : "coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+)
 
 
 export {userRouter};
