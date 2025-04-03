072de84666775aecfa379b931a6a97808a0204b8
saikishore0319 <saikishor20172017@gmail.com>
2025-04-03 10:25:32 +0530

    minor bug fixing

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -3,24 +3,23 @@ import multer from "multer";
 /* This code snippet is configuring the storage settings for file uploads using the `multer` middleware
 in a Node.js application. Here's a breakdown of what each part does: */
 const storage = multer.diskStorage({
-   /* The `destination` function in the `multer.diskStorage` configuration is specifying the directory
+  /* The `destination` function in the `multer.diskStorage` configuration is specifying the directory
    where uploaded files will be stored on the server. In this case, it is set to "../public/temp",
    which means that uploaded files will be saved in the "temp" directory within the "public"
    directory. The `cb` function is a callback that needs to be called once the destination directory
    is determined. */
-    destination: function (req, file, cb) {
-      cb(null, "../public/temp")
-    },
-    /* The `filename` function in the `multer.diskStorage` configuration is specifying how the uploaded
+  destination: function (req, file, cb) {
+    cb(null, "public/temp");
+  },
+  /* The `filename` function in the `multer.diskStorage` configuration is specifying how the uploaded
     file should be named when it is saved on the server. In this case, the function is using
     `file.originalname` to keep the original name of the uploaded file. The `cb` function is a
     callback that needs to be called once the new filename is determined. */
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-  
-  export const upload = multer({ 
-    storage,
- })
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
+export const upload = multer({
+  storage: storage,
+});
--- src/utils/coludinary.js
+++ src/utils/coludinary.js
@@ -12,14 +12,16 @@ const cloudinaryUpload = async(localFilepath)=>{
     try {
         if(!localFilepath) return null;
         //upload file to cloudinary
-       /* The line `const response = await cloudinary.uploader.upload(localFilepath, { resource_type:
-       'auto' })` is uploading a file located at the `localFilepath` to Cloudinary using the
-       Cloudinary SDK. */
+       /* The code snippet you provided is a function named `cloudinaryUpload` that uploads a file
+       located at the `localFilepath` to Cloudinary using the Cloudinary SDK. Here is a breakdown of
+       what the code is doing: */
         const response = await cloudinary.uploader.upload(localFilepath,{
             resource_type: 'auto',
         })
         //file upload done successfully
         console.log(response.url);
+        console.log(response)
+        fs.unlinkSync(localFilepath)
         return response;    
     } catch (error) {
         /* `fs.unlinkSync(localFilepath)` is a method in Node.js that synchronously removes the file
