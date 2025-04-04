f69673aaa97de8bdc31dcd0f13a47cb507263bf5
saikishore0319 <saikishor20172017@gmail.com>
2025-04-04 12:03:50 +0530

    completed user login logout

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,9 +5,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
-    const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
       throw new ApiError(401, "Unauthorized access");
@@ -26,7 +24,6 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-
-    throw new ApiError(401, error?.message || "invalid access token")
+    throw new ApiError(401, error?.message || "invalid access token");
   }
 });
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
 userRouter.route("/register").post(
@@ -17,5 +18,9 @@ userRouter.route("/register").post(
     registerUser
 )
 
+userRouter.route("/login").post(loginUser)
+
+//secured routers
+userRouter.route("/logout").post(verifyJWT, logoutUser)
 
 export {userRouter};
